d79252b9c22ace080dd408bc8b13ea1b9c7077af
Vladimir <suslovvladimir@outlook.com>
2018-05-09 22:28:30 +0300

    Setup project

---
+++ ShowBill/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShowBill.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
